e1161dd55cbb4986bceb0314426a516cd1c281b4
Jhonatas <jhonatasanic@hotmail.com>
2022-02-04 11:28:37 -0300

    exercicios referente ao dia 02 bloco 04

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -0,0 +1,73 @@
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1
+//for(let index = 0 ; index < numbers.length ; index +=1){
+//   console.log(numbers[index]);
+//}
+
+
+//2
+//for(let index = 0 ; index < numbers.length ; index +=1){
+//      console.log(numbers[index] + 1);
+//  }
+
+
+//3 e 4
+//   let soma=0;
+//    for(let index = 0 ; index < numbers.length ; index +=1){
+//       soma += numbers[index];
+//      
+//   }
+//   if (soma > 20){
+//       console.log("valor maior que 20")
+//   }
+//   else{
+//       console.log("valor menor que 20")
+//   }
+//    console.log(soma / 10);
+
+
+//5
+//let numeroMaior = 0;
+//for(let index = 0 ; index < numbers.length ; index +=1){
+//    for(let number2 of numbers){
+//        if (numbers[index] > number2 && numbers[index] > numeroMaior){
+//            numeroMaior = numbers[index];
+//        }
+//    }
+//}
+//console.log(numeroMaior);
+
+
+//6
+//for(let index = 0 ; index < numbers.length ; index +=1){
+//    if (numbers[index] % 2 == 1){
+//        console.log(numbers[index]);
+//    }
+//}
+
+
+//7
+//let numeroMenor = numbers[3] ;
+//for(let index = 0 ; index < numbers.length ; index +=1){
+//    for(let number2 of numbers){
+//        if (numbers[index] < number2 && numbers[index] < numeroMenor){
+//         numeroMenor = numbers[index];
+//        }
+//    }
+//}
+//console.log(numeroMenor);
+
+
+//8 e 9
+//let array = [];
+//for(let index = 0 ; index <= 25 ; index +=1){
+//    array.push(index);
+//}
+//for(let index2 = 0; index2 < array.length ; index2 +=1){
+//    console.log(array[index2] / 2)
+//}
+
+
+
