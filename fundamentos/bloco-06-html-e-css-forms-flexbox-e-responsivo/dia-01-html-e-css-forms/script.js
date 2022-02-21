faa777ea167728b39019c7562462a5b6781bf43d
Jhonatas <jhonatasanic@hotmail.com>
2022-02-21 11:59:22 -0300

    dia 01 bloco 06

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/script.js
@@ -0,0 +1,8 @@
+let enviar = document.getElementById("enviar");
+let main = document.getElementById("main");
+
+function defaulto(){
+    event.preventDefault()
+}
+
+enviar.addEventListener("click", defaulto)
